a67a73bb97161fb6fdc323833d52ce9fad0a0093
wobsoriano <sorianorobertc@gmail.com>
2026-04-29 23:21:22 -0700

    feat(native): add Package.swift for iOS SPM distribution

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "CapacitorClerk",
+    platforms: [.iOS(.v16)],
+    products: [
+        .library(name: "CapacitorClerk", targets: ["CapacitorClerkNative"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm", from: "8.0.0"),
+        .package(url: "https://github.com/clerk/clerk-ios", from: "2.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "CapacitorClerkNative",
+            dependencies: [
+                .product(name: "Capacitor",  package: "capacitor-swift-pm"),
+                .product(name: "Cordova",    package: "capacitor-swift-pm"),
+                .product(name: "ClerkKit",   package: "clerk-ios"),
+                .product(name: "ClerkKitUI", package: "clerk-ios"),
+            ],
+            path: "ios/Sources/CapacitorClerkNative"
+        ),
+    ]
+)
