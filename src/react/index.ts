6e60c85b3eef67f93b69b925e247e7d7a462b8b0
wobsoriano <sorianorobertc@gmail.com>
2026-04-29 21:31:34 -0700

    feat(react): export useSignInWithApple from public API

--- src/react/index.ts
+++ src/react/index.ts
@@ -26,4 +26,7 @@ export type { ClerkProviderProps } from './ClerkProvider';
 export { useSSO } from './useSSO';
 export type { StartSSOFlowParams, StartSSOFlowReturnType } from './useSSO';
 
+export { useSignInWithApple } from './useSignInWithApple';
+export type { StartAppleAuthenticationFlowParams, StartAppleAuthenticationFlowReturnType } from './useSignInWithApple';
+
 export type { TokenCache } from '../definitions';
